ba0fdd4a83e4797869f2ed408c791bae032e46d0
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2023-10-30 07:51:06 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    // Specify total number of rows and columns
+    int rows = 10, cols = 10;
+    for (int r = 1; r <= rows; r++)
+    {
+        for (int c = 1; c <= cols; c++)
+        {
+            if (r==1 || c==cols/2 || ((c==1)&&(r>=rows/2)) || ((r==rows) &&(c<=cols/2)))
+                printf("%2d",r);
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
